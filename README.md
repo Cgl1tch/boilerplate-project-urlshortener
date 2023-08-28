20c0a6e838b6a3efe3680e31ef9ffd89cb5ea124
Cgl1tch <42437908+Cgl1tch@users.noreply.github.com>
2023-08-28 06:35:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# URL Shortener Microservice
+
+FreeCodeCamp project for the Back-end development and APIs certification
---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.post('/api/shorturl', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
