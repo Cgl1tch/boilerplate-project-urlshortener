aa4a2e58fa251670f5d35a3676077c4b921450b5
Cgl1tch <42437908+Cgl1tch@users.noreply.github.com>
2023-09-08 05:59:35 +0000

    feat: add urlshortener solution

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const app = express();
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 const client = new MongoClient(process.env.DB_URI);
+const db = client.db("urlshortener");
+const urls = db.collection("urls")
+const dns = require("dns");
+const urlparser = require("url");
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -22,9 +25,29 @@ app.get('/', function(req, res) {
 
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
-  res.json({ greeting: 'hello API' });
+  const url = req.body.url;
+  const dnslookup = dns.lookup(urlparser.parse(url).hostname, async (err, address) => {
+    if (!address){
+      res.json({error: "invalid URL"})
+    }else{
+      const urlCount = await urls.countDocuments({})
+      const urlDoc = {
+        url,
+        short_url: urlCount
+      }
+      const result = await urls.insertOne(urlDoc)
+      res.json({
+        original_url: url,
+        short_url: urlCount
+      })
+    }
+  })
 });
-
+app.get('/api/shorturl/:short_url', async (req, res) => {
+  const shorturl = req.params.short_url;
+  const urlDoc = await urls.findOne({short_url: +shorturl});
+  res.redirect(urlDoc.url);
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
